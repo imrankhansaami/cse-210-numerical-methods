c0a706676f5d542fca2dbffd9c3f6df8927f4fa2
Imran Khan <imran2013samirkhan@gmail.com>
2025-01-11 08:48:02 +0600

    Add files via upload

---
+++ Regula Falsi Method.m
@@ -0,0 +1,27 @@
+f = @(x) x^3 - x - 2;
+a = 1;
+b = 2;
+tol = 1e-4;
+n = 20;
+
+for i = 1:n
+  c = (a * f(b) - b * f(a)) / (f(b) - f(a));
+  fprintf('Iteration: %d: c = %.6f, f(c) = %.6f \n', i, c, f(c));
+
+  if abs(f(c)) < tol
+    fprintf('Root: %.6f after %d iteration \n', c, i);
+
+    break;
+  end
+
+  if f(a) * f(c) < 0
+    b = c
+  else
+    a = c
+  end
+end
+
+if i == n
+  fprintf('Root not found');
+
+  end
